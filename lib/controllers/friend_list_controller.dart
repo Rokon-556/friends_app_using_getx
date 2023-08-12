573b604500bb9e37442622bd2d26fd41d3b1072a
Rokon-556 <rokonrahman556@gmail.com>
2023-08-12 18:50:03 +0600

    model upgradation

--- lib/controllers/friend_list_controller.dart
+++ lib/controllers/friend_list_controller.dart
@@ -6,42 +6,15 @@ import 'package:get/get.dart';
 
 class FriendListController extends GetxController {
   RxBool isDataLoading = false.obs;
-  List<Result> friendList = <Result>[];
-  var users = <Result>[].obs;
+  var friendList = <Result>[].obs;
 
   @override
   void onInit() {
     super.onInit();
-    // fetchRandomUsers();
-    makeApiCalls(10);
-
+    getFriendList();
   }
 
   Future<void> getFriendList() async {
-    isDataLoading.value = true;
-      const url = 'https://randomuser.me/api/';
-      final dio = Dio();
-      final response = await dio.get(url);
-
-       try {
-         if (response.statusCode == 200) {
-           var data = response.data['results'] as List<dynamic>;
-           log('friend:$data');
-           if (data.isNotEmpty) {
-             for (var friend in data) {
-               friendList.add(Result.fromJson(friend));
-             }
-           }
-           isDataLoading.value = false;
-         }
-         isDataLoading.value = false;
-       } catch (e) {
-         log(e.toString());
-     }
-    log("friendList:${friendList.length}");
-  }
-
-  Future<void> fetchRandomUsers() async {
     try {
       isDataLoading.value =true;
       final dio = Dio();
@@ -49,17 +22,11 @@ class FriendListController extends GetxController {
       final results = response.data['results'] as List<dynamic>;
       log('friend:$results');
       final List<Result> userList = results.map((user) => Result.fromJson(user)).toList();
-      users.value = userList;
+      friendList.value = userList;
+      log('user value:${friendList.length}');
       isDataLoading.value = false;
     } catch (error) {
       log('Error fetching users: $error');
-    }
-  }
-
-
-  Future<void> makeApiCalls(int times) async {
-    for (int i = 0; i < times; i++) {
-      await getFriendList();
     }
   }
 }
